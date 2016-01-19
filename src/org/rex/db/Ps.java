aed8004bef3747214e963c2de693cdf09ffdfefb
Administrator <Administrator@z-PC>
2016-01-19 09:26:20 +0800

    rollback night

--- src/org/rex/db/Ps.java
+++ src/org/rex/db/Ps.java
@@ -886,15 +886,28 @@ public class Ps {
 	}
 	//-------------------------------------------toString
 	public String toString() {
-		StringBuffer sb = new StringBuffer();
-		for (int i = 0; i < parameters.size(); i++) {
-			sb.append(i)
-				.append(":")
-				.append(parameters.get(i));
-			
-			if(i != parameters.size() -1)
-				sb.append(",");
+		List values = new ArrayList();
+		List<String> sqlTypes = new ArrayList<String>();
+		List<String> paramTypes = new ArrayList<String>();
+		
+		for (SqlParameter parameter : parameters) {
+			values.add(parameter.getValue());
+			sqlTypes.add(JdbcUtil.getNameByType(parameter.getSqlType()));
+			if(parameter instanceof SqlInOutParameter)
+				paramTypes.add("INOUT");
+			else if(parameter instanceof SqlOutParameter)
+				paramTypes.add("OUT");
+			else
+				paramTypes.add("IN");
 		}
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("values: ")
+			.append(values)
+			.append(", sql types: ")
+			.append(sqlTypes)
+			.append(", parameter types: ")
+			.append(paramTypes);
 		return sb.toString();
 	}
 	
